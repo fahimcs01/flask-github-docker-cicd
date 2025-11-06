33e156d17ae02ccbadca0bdd39cd4acadbc17ba8
Mohammad Fahim Khan <141926442+Fahimashu@users.noreply.github.com>
2025-11-06 16:14:49 +0530

    updated to code

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "✅ Hello from CI/CD Pipeline using GitHub Actions & Docker!"
+    return "✅ Hello from CI/CD Pipeline using GitHub Actions & Docker!  This is Fahim"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
